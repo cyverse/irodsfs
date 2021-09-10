5297a0fd9474c76454d7f5318c1259b5c2c0d025
Illyoung Choi <iychoi@email.arizona.edu>
2021-09-09 17:42:55 -0700

    Add mutex to filemeta updater to prevent race condition

--- pkg/irodsfs/filemeta_updater.go
+++ pkg/irodsfs/filemeta_updater.go
@@ -1,9 +1,12 @@
 package irodsfs
 
+import "sync"
+
 // FileMetaUpdater holds updates on file metadata temporarily.
 // So subsequent fs operations find them to refresh metadata.
 type FileMetaUpdater struct {
 	UpdatedFiles map[int64]*FileMetaUpdated
+	Mutex        sync.Mutex
 }
 
 // FileMetaUpdated holds information that is updated
@@ -24,22 +27,34 @@ func (updater *FileMetaUpdater) Add(inode int64, path string) {
 		Path: path,
 	}
 
+	updater.Mutex.Lock()
+	defer updater.Mutex.Unlock()
+
 	updater.UpdatedFiles[inode] = u
 }
 
 // Get returns the update on a file/directory with the inode
 func (updater *FileMetaUpdater) Get(inode int64) (*FileMetaUpdated, bool) {
+	updater.Mutex.Lock()
+	defer updater.Mutex.Unlock()
+
 	u, ok := updater.UpdatedFiles[inode]
 	return u, ok
 }
 
 // Delete returns the update on a file/directory with the inode
 func (updater *FileMetaUpdater) Delete(inode int64) {
+	updater.Mutex.Lock()
+	defer updater.Mutex.Unlock()
+
 	delete(updater.UpdatedFiles, inode)
 }
 
 // Pop pops the update on a file/directory with the inode
 func (updater *FileMetaUpdater) Pop(inode int64) (*FileMetaUpdated, bool) {
+	updater.Mutex.Lock()
+	defer updater.Mutex.Unlock()
+
 	if u, ok := updater.UpdatedFiles[inode]; ok {
 		delete(updater.UpdatedFiles, inode)
 		return u, ok
